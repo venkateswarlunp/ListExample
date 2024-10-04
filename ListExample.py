0099e84011364310556e839a439de27e586808b0
venkateswarlunp <khannaitist@gmail.com>
2024-10-04 08:33:17 +0530

    Added ListExampel File

---
+++ ListExample.py
@@ -0,0 +1,12 @@
+
+# Python code for List Example
+# To print all positive numbers in a given range 
+
+a=-4;b=5
+
+out=[i for i in range(a,b+1) if i>0] 
+
+# print the all positive numbers 
+print(*out) 
+
+# End-Of-Code
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# ListExample
+#@ ReadMe File
